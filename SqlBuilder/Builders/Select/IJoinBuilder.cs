7d9a51c84a64526436445ef121bd9e6e69a3694d
smistry7 <shyammistry96@gmail.com>
2020-11-10 20:03:28 +0000

    docs: add more xml documentation

--- SqlBuilder/Builders/Select/IJoinBuilder.cs
+++ SqlBuilder/Builders/Select/IJoinBuilder.cs
@@ -2,6 +2,17 @@
 {
     public interface IJoinBuilder
     {
+        /// <summary>
+        /// The On Method
+        /// 
+        /// Use this method after a Join method to set your on clause
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// builder.Select("*").From("table").LeftJoin("otherTable").On("a = b");
+        /// </code>
+        /// </example>
+        /// <param name="on">String representation of the on clause</param>
         IFromBuilder On(string on);
     }
 }
--- SqlBuilder/Builders/Select/IWhereBuilder.cs
+++ SqlBuilder/Builders/Select/IWhereBuilder.cs
@@ -9,14 +9,48 @@
         /// </summary>
         /// <param name="where">string representation of the and expression</param>
         IWhereBuilder And(string where);
-        
+        /// <summary>
+        /// The And method
+        /// 
+        /// Use this method after a where to add an AND expression
+        /// </summary>
+        /// <param name="field">Field/column to compare</param>
+        /// <param name="value">Value to set it equal to</param>
         IWhereBuilder And(string field, object value);
-
+        /// <summary>
+        /// The Or Method
+        /// 
+        /// Use this method after a where to add an OR expression
+        /// </summary>
+        /// <param name="where">string representation of the or expression</param>
         IWhereBuilder Or(string where);
-
+        /// <summary>
+        /// The Or method
+        /// 
+        /// Use this method after a where to add an OR expression
+        /// </summary>
+        /// <param name="field">Field/column to compare</param>
+        /// <param name="value">Value to set it equal to</param>
         IWhereBuilder Or(string field, object value);
-
+        /// <summary>
+        /// The OrderBy Method
+        /// 
+        /// Use this method to add an Order By expression in a SQL query
+        /// </summary>
+        /// <param name="orderBy">field/column to order by</param>
+        /// <returns></returns>
         IOrderByBuilder OrderBy(string orderBy);
+        /// <summary>
+        /// The Paginate Method
+        /// </summary>
+        /// <example>
+        /// <code>
+        /// builder.Select("*").From("table").Paginate(4, 20);
+        /// </code>
+        /// </example>
+        /// <param name="pageNumber">integer of the page number to retrieve</param>
+        /// <param name="pageSize">integer of the page size</param>
+        /// <returns></returns>
         ISqlQueryBuilder Paginate(int pageNumber, int pageSize);
     }
 }
