7daaf2211b1d16ef46e16ffed56f971c06f65854
DESKTOP-QNDHEEA\shyam <shyammistry96@gmail.com>
2020-11-26 21:10:30 +0000

    test: organise tests and add extension method tests

--- SqlBuilder.Tests/Builders/InsertBuilderTests.cs
+++ SqlBuilder.Tests/Builders/InsertBuilderTests.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 using BeeseChurger.SqlBuilder.Misc;
 using Xunit;
 
-namespace BeeseChurger.SqlBuilder.Tests
+namespace BeeseChurger.SqlBuilder.Tests.Builders
 {
     public class InsertBuilderTests
-    { 
+    {
         [Fact]
         public void BuildShouldReturnCorrectString()
         {
-            
+
             var dt = DateTime.Now;
             var builder = InsertBuilder
                 .InsertInto("table", new string[] { "col1", "col2", "col3" })
                 .Values(new object[] { 1, "skdajhfla", dt });
 
-            builder.Build().Should().Be($"INSERT INTO table (col1, col2, col3 ) VALUES (1, 'skdajhfla', {dt.ToSqlParameter()} ) ;"); 
+            builder.Build().Should().Be($"INSERT INTO table (col1, col2, col3 ) VALUES (1, 'skdajhfla', {dt.ToSqlParameter()} ) ;");
         }
         [Fact]
         public void ShouldBuildCorrectStringMultipleValue()
---
+++ SqlBuilder.Tests/ExtensionMethodTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+using BeeseChurger.SqlBuilder.Misc;
+using FluentAssertions;
+
+namespace BeeseChurger.SqlBuilder.Tests
+{
+    public class ExtensionMethodTests
+    {
+        [InlineData("blahblah", "'blahblah'")]
+        [InlineData(123, "123")]
+        [Theory]
+        public void ToSqlParameter_ShouldReturnCorrectly(object input, string expected)
+        {
+            input.ToSqlParameter().Should().Be(expected);
+        }
+    }
+}
